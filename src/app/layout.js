12574c48e3fc6d5fbb8bd91903c1995e729089fb
manishPatel1997 <manishpatel.mp1997@gmail.com>
2023-12-16 12:27:13 +0530

    layout and nested layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,9 +4,13 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
- return (
+  return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header style={{ background: "yellowgreen", padding: "1rem" }}>layout header</header>
+        {children}
+        <footer style={{ background: "lightcyan", padding: "1rem" }}>layout footer</footer>
+      </body>
     </html>
   )
 }
---
+++ src/app/product/[productId]/layout.js
@@ -0,0 +1,8 @@
+export default function RootLayout({ children }) {
+    return (
+        <>
+            <h1>im productid layout :: nested layout</h1>
+            {children}
+        </>
+    )
+}
