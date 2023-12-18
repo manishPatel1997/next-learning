42719211d1103efea99b532a1b5ecd753b5f2204
manishPatel1997 <manishpatel.mp1997@gmail.com>
2023-12-18 17:56:05 +0530

    dynamic generateMetadata

--- src/app/product/[productId]/page.js
+++ src/app/product/[productId]/page.js
@@ -1,5 +1,11 @@
-export default function ProductId({params}) {
+export default function ProductId({ params }) {
   return (
     <div>ProductId : {params.productId} </div>
   )
+}
+
+export const generateMetadata = ({ params }) => {
+  return {
+    title: `Product ${params.productId}`,
+  }
 }
